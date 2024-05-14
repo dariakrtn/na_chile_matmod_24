8052d44745e4050296b97aad53373a76afadf94b
IivaNsh <ivansher0226@gmail.com>
2024-05-14 16:12:03 +0300

    interface polishing

--- module_2/app/converter.py
+++ module_2/app/converter.py
@@ -44,7 +44,7 @@ def get_interesting_moments(pgn_file):
 
 
 
-def create_intersting_clips(video_bytes, pgn_str, start_time):
+def create_intersting_clips(video_file, pgn_str, start_time):
     
     #pgn_str = pgn_file.read().decode('utf-8')
     with open("./Ahackaton/Belgrade2024/Round_1.pgn.pgn", "r", encoding="utf-8") as f:
@@ -56,9 +56,8 @@ def create_intersting_clips(video_bytes, pgn_str, start_time):
     with open("./Ahackaton/Belgrade2024/Round_1.json", "r", encoding="utf-8") as f:
         res_json = json.loads(f.read())
    
-    df = pars.pars_pgn(pgn_str, res_json)[0]
+    df = pars.pars_pgn(pgn_str, res_json)[0][0]
 
-    
     df = df.loc[df['comment'].notna()]
     df.reset_index(drop=True, inplace=True)
     comments = df["comment"].values
@@ -89,3 +88,51 @@ def create_intersting_clips(video_bytes, pgn_str, start_time):
     
 
     return videos, comments
+
+
+
+
+def create_shots(video_file, pgn_str, start_time):
+    
+    #pgn_str = pgn_file.read().decode('utf-8')
+    with open("./Ahackaton/Belgrade2024/Round_1.pgn.pgn", "r", encoding="utf-8") as f:
+        pgn_str = f.read()
+    #print(pgn_str)
+
+    
+    #res_json = api_promt.comm_gpt(pgn_str)
+    with open("./Ahackaton/Belgrade2024/Round_1.json", "r", encoding="utf-8") as f:
+        res_json = json.loads(f.read())
+   
+    df = pars.pars_pgn(pgn_str, res_json)[0][0]
+
+    df = df.loc[df['comment'].notna()]
+    df.reset_index(drop=True, inplace=True)
+    comments = df["comment"].values
+
+    #video_data = video_file
+    # video_data = st.session_state.video_file.read()
+
+    #write_bytesio_to_file(temp_file_to_save, video_data)
+
+
+    # turns = [3, 5, 10] # TODO: generate automaticly
+    # video_clips = VideoClips.multi_timing_crop(video_file, df, start_time)
+    # TODO: convert vei_clips to videos in bytes
+    
+    
+    # TEMP
+    videos = [] 
+
+    path = "data/videos"
+    files = [f for f in listdir(path) if isfile(join(path, f))]
+    for file in files:
+        with open(f"{path}/{file}", "rb") as f:
+            videos.append(f.read())
+    
+
+    #video_file_converted = video_file
+    
+
+    return videos, comments
+
