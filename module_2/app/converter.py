bf6a79c5b91a3efccfabb5442609e3745ae563a0
IivaNsh <ivansher0226@gmail.com>
2024-05-14 11:39:06 +0300

    converter testing

--- module_2/app/converter.py
+++ module_2/app/converter.py
@@ -7,38 +7,55 @@ import api_promt
 def get_interesting_moments(pgn_file):
     
     # TODO: pgn ifle analisys & extract interesting moments as Dataframe
+    pgn_str = pgn_file.read()
+    print(pgn_str)
+
+
+    res_json = api_promt.comm_gpt(pgn_str)
+
+    df = pars.pars_pgn(pgn_file.read(), res_json)[0]
+
+
 
     return 0
 
 
 
-def convert_video(video_file, pgn_file):
+
+# func to save BytesIO on a drive
+def write_bytesio_to_file(filename, bytesio):
+    """
+    Write the contents of the given BytesIO to a file.
+    Creates the file or overwrites the file if it does
+    not exist yet. 
+    """
+    with open(filename, "wb") as outfile:
+        # Copy the BytesIO stream to the output file
+        outfile.write(bytesio.getbuffer())
+
+temp_file_to_save = "../data/videos/tmp_video.mp4"
+
+def convert_video(video_file, pgn_file, start_time):
     
 
     pgn_str = pgn_file.read()
     print(pgn_str)
 
-    
+
     res_json = api_promt.comm_gpt(pgn_str)
 
     df = pars.pars_pgn(pgn_file.read(), res_json)[0]
+    interesting_moments = df.loc[df['comment'].notna()].values()
+    print(interesting_moments)
 
-    # TODO: turns
-    # turns = ... 
+    #video_data = video_file
+    # video_data = st.session_state.video_file.read()
+
+    #write_bytesio_to_file(temp_file_to_save, video_data)
 
 
-
-    video_data = st.session_state.video_file.read()
-
-    # Create a BytesIO object to hold the video data
-    video_stream = BytesIO(video_data)
-
-    # Load the video from the BytesIO object
-    video_clip = VideoFileClip(video_stream)
-    # Now you have a VideoClip object called video_clip
-
-    turns = [3, 5, 10] # TODO: generate automaticly
-    video_clips = multi_timing_crop(video_clip, turns)
+    # turns = [3, 5, 10] # TODO: generate automaticly
+    #video_clips = multi_timing_crop(video_clip, df, turns, start_time)
 
     video_file_converted = video_file
-    return video_file_converted 
+    return video_file_converted
--- module_2/app/video_file_to_videoClip_test.py
+++ module_2/app/video_file_to_videoClip_test.py
@@ -9,14 +9,16 @@ if "video_file" in st.session_state and st.session_state.video_file:
     
     video_data = st.session_state.video_file.read()
 
-    #print(video_data)
+    # print(video_data)
 
     # Create a BytesIO object to hold the video data
     video_stream = BytesIO(video_data)
 
+    print(video_stream)
     # Load the video from the BytesIO object
     video_clip = VideoFileClip(video_stream)
     # Now you have a VideoClip object called video_clip
     
     new_video_clip = video_clip.cut(5, 10)
-    
+
+    clip.preview(fps = 20)
